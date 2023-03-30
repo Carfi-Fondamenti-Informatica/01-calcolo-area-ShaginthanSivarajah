cd48b888a4781dbf4c966ac244e13263ea5eeb98
ShaginthanSivarajah <128066401+ShaginthanSivarajah@users.noreply.github.com>
2023-03-30 16:55:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
     float a=0, b=0, c=0;
     float Triangolo=0, Quadrato=0, Rettangolo=0, Trapezio=0;
-  cout << "Inserisci tre numeri reali"\n";
-  cin >> a >> b >> c;
-  Triangolo= (a+b)/2;
-  Quadrato= a*a;
-  Rettangolo= a*b;
-  Trapezio= ((a+b)*c)/2;
-  cout << Triangolo << endl;
-  cout << Quadrato << endl;
-  cout << Rettangolo << endl;
-  cout << Trapezio << endl;
+    cout << "Inserisci tre numeri reali"\n";
+    cin >> a >> b >> c;
+    Triangolo= (a+b)/2;
+    Quadrato= a*a;
+    Rettangolo= a*b;
+    Trapezio= ((a+b)*c)/2;
+    cout << Triangolo << endl;
+    cout << Quadrato << endl;
+    cout << Rettangolo << endl;
+    cout << Trapezio << endl;
   return 0;
 }
